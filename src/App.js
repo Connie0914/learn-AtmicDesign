0b4f4d1c23789c7b5237b07296474d690425324d
こにー | Connie <71988605+Connie0914@users.noreply.github.com>
2021-11-10 22:07:23 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "こにまる",
+  image: "https://source.unsplash.com/sR0cTmQHPug",
+  email: "test@example.com",
+  phone: "0000-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -9,8 +21,8 @@ export default function App() {
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <br />
-      <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
